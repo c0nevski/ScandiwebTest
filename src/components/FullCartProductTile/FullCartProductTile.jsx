09d4eb69e944e26fb73aad4d891d10b62a80b80f
c0nevski <z.conevski@yahoo.com>
2021-11-25 18:17:05 +0100

    added gallery slider in full cart page

--- src/components/FullCartProductTile/FullCartProductTile.jsx
+++ src/components/FullCartProductTile/FullCartProductTile.jsx
@@ -1,20 +1,52 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { adjustQty, removeFromCart, updateAttributesInCart } from "../../redux/Shopping/shopping-actions";
+import {
+  adjustQty,
+  removeFromCart,
+  updateAttributesInCart,
+} from "../../redux/Shopping/shopping-actions";
 import "./FullCartProductTile.scss";
 
 class FullCartProductTile extends Component {
-
   state = {
     product: this.props.product,
     image: this.props.product.gallery[0],
+    imagePosition: 0,
   };
+
+  galleryBackBtn = () => {
+    const { gallery } = this.props.product;
+    if(this.state.imagePosition === 0) {
+      this.setState({
+        imagePosition: gallery.length - 1
+      });
+    } else {
+      this.setState({
+        imagePosition: this.state.imagePosition - 1
+      });
+    }
+  }
+
+  galleryNextBtn = () => {
+    const { gallery } = this.props.product;
+    if(this.state.imagePosition === gallery.length - 1) {
+      this.setState({
+        imagePosition: 0
+      });
+    } else {
+      this.setState({
+        imagePosition: this.state.imagePosition + 1
+      });
+    }
+  }
 
   productPrice = () => {
     const productPrice = this.props.product.prices.find(
       (price) => price.currency === this.props.currency.selectedCurrency
     );
-    const currency = this.props.currency.list.find(c => c.name === this.props.currency.selectedCurrency);
+    const currency = this.props.currency.list.find(
+      (c) => c.name === this.props.currency.selectedCurrency
+    );
     return `${currency.symbol} ${productPrice.amount}`;
   };
 
@@ -30,7 +62,10 @@ class FullCartProductTile extends Component {
       }
     });
 
-    const updatedProduct = {...this.state.product, attributes: updatedAttributes};
+    const updatedProduct = {
+      ...this.state.product,
+      attributes: updatedAttributes,
+    };
 
     this.updateAttributesInCart(updatedProduct);
     this.setState({ product: updatedProduct });
@@ -38,32 +73,28 @@ class FullCartProductTile extends Component {
 
   updateAttributesInCart = (product) => {
     this.props.updateProductAttributesInCart(product);
-  }
+  };
 
   adjustQuantity = (value) => {
-    if(value <= 0) {
+    if (value <= 0) {
       this.removeFromCart(this.state.product);
     } else {
       this.props.adjustQty(this.state.product, value);
       this.setState({
-        product: {...this.state.product, qty: value}
+        product: { ...this.state.product, qty: value },
       });
     }
-  }
-
-
+  };
 
   removeFromCart = (product) => {
     this.props.removeFromCart(product);
-  }
+  };
 
   swapGalleryImage = () => {
-    const imagesCount = this.props.product.gallery.length;
     this.setState({
-      image: this.props.product.gallery
+      image: this.props.product.gallery,
     });
-  }
-  
+  };
 
   render() {
     const { product } = this.props;
@@ -86,8 +117,25 @@ class FullCartProductTile extends Component {
                         return (
                           <button
                             key={option.id}
-                            onClick={() => this.selectProductAttribute(attr.id, option.displayValue)}
-                            style={{ backgroundColor: `${option.value}`,borderWidth: `${ attr.selectedVal === option.displayValue ? '3px' : '1px' }` , opacity: `${ attr.selectedVal === option.displayValue ? '1' : '.3' }`}}
+                            onClick={() =>
+                              this.selectProductAttribute(
+                                attr.id,
+                                option.displayValue
+                              )
+                            }
+                            style={{
+                              backgroundColor: `${option.value}`,
+                              borderWidth: `${
+                                attr.selectedVal === option.displayValue
+                                  ? "3px"
+                                  : "1px"
+                              }`,
+                              opacity: `${
+                                attr.selectedVal === option.displayValue
+                                  ? "1"
+                                  : ".3"
+                              }`,
+                            }}
                             className={`full-cart-tile__option full-cart-tile__option--swatch ${
                               attr.selectedVal === option.id
                                 ? "full-cart-tile__option--selected"
@@ -99,7 +147,12 @@ class FullCartProductTile extends Component {
                         return (
                           <button
                             key={option.id}
-                            onClick={() => this.selectProductAttribute(attr.id, option.displayValue)}
+                            onClick={() =>
+                              this.selectProductAttribute(
+                                attr.id,
+                                option.displayValue
+                              )
+                            }
                             className={`full-cart-tile__option ${
                               attr.selectedVal === option.displayValue
                                 ? "full-cart-tile__option--selected"
@@ -119,16 +172,28 @@ class FullCartProductTile extends Component {
           <div className="col col--right">
             <div className="row">
               <div className="col col--buttons">
-                <button onClick={() => this.adjustQuantity(this.state.product.qty + 1)} className="full-cart-tile__btn full-cart-tile__btn--selected">
+                <button
+                  onClick={() =>
+                    this.adjustQuantity(this.state.product.qty + 1)
+                  }
+                  className="full-cart-tile__btn full-cart-tile__btn--selected"
+                >
                   +
                 </button>
                 <span className="full-cart-tile__qty">{product.qty}</span>
-                <button onClick={() => this.adjustQuantity(this.state.product.qty - 1)} className="full-cart-tile__btn full-cart-tile__btn--selected">
+                <button
+                  onClick={() =>
+                    this.adjustQuantity(this.state.product.qty - 1)
+                  }
+                  className="full-cart-tile__btn full-cart-tile__btn--selected"
+                >
                   -
                 </button>
               </div>
               <div className="col col--image full-cart-tile__image">
-                <img src={this.state.image} alt={product.name} />
+                <span onClick={this.galleryBackBtn} className="material-icons-outlined arrow-left">chevron_left</span>
+                <img src={product.gallery[this.state.imagePosition]} alt={product.name} />
+                <span onClick={this.galleryNextBtn} className="material-icons-outlined arrow-right">chevron_right</span>
               </div>
             </div>
           </div>
@@ -144,13 +209,16 @@ const mapStateToProps = (state) => {
   };
 };
 
-
 const mapDispatchToProps = (dispatch) => {
   return {
-    updateProductAttributesInCart: (product) => dispatch(updateAttributesInCart(product)),
+    updateProductAttributesInCart: (product) =>
+      dispatch(updateAttributesInCart(product)),
     adjustQty: (product, value) => dispatch(adjustQty(product, value)),
     removeFromCart: (product) => dispatch(removeFromCart(product)),
   };
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(FullCartProductTile);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(FullCartProductTile);
